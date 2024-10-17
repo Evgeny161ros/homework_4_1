9e2e278f9ca635845b82007ab827dfe8aa132318
EvgenyShishkin <rostow.papa.2015@gmail.com>
2024-10-17 22:23:01 +0300

    Add files via upload

---
+++ module_4_1/fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    if second != 0:
+        a = first / second
+        return a
+    elif second == 0:
+        string_ = 'Ошибка'
+        return string_
+
+
+# result = divide(3, 0) # Проверка работы функции.
+# print(result)
---
+++ module_4_1/true_math.py
@@ -0,0 +1,24 @@
+from math import inf
+def divide(first, second):
+    if second != 0:
+        a = first / second
+        return a
+    elif second == 0:
+        return inf
+
+
+# result = divide(3, 0) # Проверка работы функции.
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
